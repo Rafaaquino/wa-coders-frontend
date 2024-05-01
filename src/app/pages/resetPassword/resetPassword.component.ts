dc15940d141c487ac834d4841f36a0be04e0aa2e
Your NameRafael <rafaaquino28@gmail.com>
2024-04-30 23:39:22 -0300

    feat: validacoes de resetar a senha

--- src/app/pages/resetPassword/resetPassword.component.ts
+++ src/app/pages/resetPassword/resetPassword.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
 import { ForgotPasswordService } from 'src/app/services/forgot-password.service';
 
 @Component({
@@ -17,10 +18,10 @@ export class ResetPasswordComponent implements OnInit {
   constructor(
     private router: Router,
     private _forgotPassService: ForgotPasswordService,
+    private _messageService: MessageService,
     private fb: FormBuilder
   ) {
-    this.recoveryPassForm = fb.group({
-      email: ['', [Validators.required, Validators.email]],
+    this.recoveryPassForm = this.fb.group({
       newPassword: ['', [Validators.required, Validators.minLength(8)]],
       mathPassword: ['', [Validators.required, Validators.minLength(8)]],
     });
@@ -34,8 +35,9 @@ export class ResetPasswordComponent implements OnInit {
     }
 
     if (this.recoveryPassForm.valid) {
+      const email = localStorage.getItem('email');
       this._forgotPassService
-        .resetPassword(this.recoveryPassForm.value)
+        .resetPassword(this.recoveryPassForm.value, email)
         .subscribe({
           next: this.onSubmitSuccess.bind(this),
           error: this.onSubmitError.bind(this),
@@ -45,10 +47,20 @@ export class ResetPasswordComponent implements OnInit {
 
   onSubmitSuccess(res) {
     console.log(res);
+    localStorage.removeItem('email');
     this.router.navigateByUrl('/dashboard');
   }
 
   onSubmitError(error) {
     console.log(error);
+    this.show(error.error.message);
+  }
+
+  show(error: string) {
+    this._messageService.add({
+      severity: 'error',
+      summary: '',
+      detail: error,
+    });
   }
 }
