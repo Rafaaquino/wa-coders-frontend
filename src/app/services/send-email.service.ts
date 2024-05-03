b5c0ac2d19454f24ef8e6689f6cf9ccd5435a90b
Your NameRafael <rafaaquino28@gmail.com>
2024-05-03 20:59:28 -0300

    feat: service quote

--- src/app/services/send-email.service.ts
+++ src/app/services/send-email.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ISendMail } from '../models/sendMail.interface';
 import { environment } from 'src/environments/environment';
+import { IQuote } from '../models/client.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -14,6 +15,13 @@ export class SendEmailService {
     return this.http.post<ISendMail>(
       environment.host_api + `/emails/send-email`,
       email
+    );
+  }
+
+  sendMailQuote(quote: IQuote): Observable<IQuote> {
+    return this.http.post<IQuote>(
+      environment.host_api + `/emails/send-quotes`,
+      quote
     );
   }
 }
