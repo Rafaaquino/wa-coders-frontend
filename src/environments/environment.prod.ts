f8dfd73aa1ee96a0ac5bce29fc75e437599a1283
Your NameRafael <rafaaquino28@gmail.com>
2024-05-08 02:02:35 -0300

    chore: env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  host_api: 'https://www.wacoders.com.br/api/v1',
+  host_api: 'https://wa-coders-backend-api-7f146f8b0faa.herokuapp.com/api/v1',
 };
