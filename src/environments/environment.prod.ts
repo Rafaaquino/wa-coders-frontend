2ab39745220530d1a65142966ecd74758f9f49f0
Your NameRafael <rafaaquino28@gmail.com>
2025-11-19 22:21:34 -0300

    chore: env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  host_api: 'https://wa-coders-backend-api.vercel.app/api/v1', //VERCEL
+  host_api:
+    'wa-coders-backend-e1sn2k49w-rafaaquinos-projects.vercel.app/api/v1', //VERCEL
+  //host_api: 'https://wa-coders-backend-api.vercel.app/api/v1', //VERCEL
   //host_api: 'https://wa-coders-backend-api-7f146f8b0faa.herokuapp.com/api/v1', HEROKU
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,8 @@
 
 export const environment = {
   production: false,
-  host_api: 'https://wa-coders-backend-api.vercel.app/api/v1', //VERCEL
+  host_api:
+    'wa-coders-backend-e1sn2k49w-rafaaquinos-projects.vercel.app/api/v1', //VERCEL
+  //host_api: 'https://wa-coders-backend-api.vercel.app/api/v1', //VERCEL
   //host_api: 'https://wa-coders-backend-api-7f146f8b0faa.herokuapp.com/api/v1', HEROKU
 };
